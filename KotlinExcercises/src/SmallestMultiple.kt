2fb1503b34281b88888c8fd46bee14757191145f
Alan Gustavo <60892674+AlanGustav0@users.noreply.github.com>
2020-07-09 08:14:48 -0300

    New file and update files

--- KotlinExcercises/src/SmallestMultiple.kt
+++ KotlinExcercises/src/SmallestMultiple.kt
@@ -3,15 +3,15 @@ fun main() {
     val go: Boolean = true
     var number: Int = 1
 
-    while (go){
+    while (go) {
 
         for value in 1..20 {
-            if (value % number == 0){
-                sum ++;
+            if (value % number == 0) {
+                sum++;
             }
         }
 
-        if sum == 20{
+        if (sum == 20) {
             print(number)
             break;
         }
